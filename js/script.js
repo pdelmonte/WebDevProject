e68e7f68c53abefc6d03d7615c02cc4aaf11e67a
pedrodelmonte <pdelmonte@gmail.com>
2017-01-23 17:01:14 +0100

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,78 @@
+$(document).ready(function () {
+
+var menu = $('.menu');
+var origOffsetY = menu.offset().top;
+
+function scroll() {
+    if ($(window).scrollTop() >= origOffsetY) {
+        $('.menu').addClass('navbar-fixed-top');
+        $('.divisas').addClass('menu-padding');
+
+    } else {
+        $('.menu').removeClass('navbar-fixed-top');
+        $('.divisas').removeClass('menu-padding');
+
+    }
+   }
+  document.onscroll = scroll;
+
+  /* activate the carousel */
+   $("#modal-carousel").carousel({interval:false});
+
+   /* change modal title when slide changes */
+   $("#modal-carousel").on("slid.bs.carousel",       function () {
+        $(".modal-title")
+        .html($(this)
+        .find(".active img")
+        .attr("title"));
+   });
+
+   /* when clicking a thumbnail */
+   $(".row .thumbnail").click(function(){
+    var content = $(".carousel-inner");
+    var title = $(".modal-title");
+
+    content.empty();
+    title.empty();
+
+  	 var id = this.id;
+     var repo = $("#img-repo .item");
+     var repoCopy = repo.filter("#" + id).clone();
+     var active = repoCopy.first();
+
+    active.addClass("active");
+    title.html(active.find("a").attr("title"));
+  	content.append(repoCopy);
+
+    // show the modal
+  	$("#modal-gallery").modal("show");
+  });
+
+  $(function() {
+    // validate
+    $("#contact").validate({
+        // Set the validation rules
+        rules: {
+            name: "required",
+            email: {
+                required: true,
+                email: true
+            },
+            message: "required",
+        },
+        // Specify the validation error messages
+        messages: {
+            name: "Please enter your name",
+            email: "Please enter a valid email address",
+            message: "Please enter a message",
+        },
+        // submit handler
+        submitHandler: function(form) {
+          //form.submit();
+           $(".message").show();
+           $(".message").fadeOut(4500);
+        }
+    });
+  });
+
+});
